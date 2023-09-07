25f86331de18094f9f9780675cd1300d7a47c31a
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-09-07 13:31:06 +0545

    seeder implemented in node

---
+++ lib/seeder.ts
@@ -0,0 +1,39 @@
+import * as readlineSync from 'readline-sync'
+import * as fs from 'fs'
+import { exec } from 'child_process'
+
+const writeMigrationFile = (tableName: string, iFileName: string) => {
+  const cwd = process.cwd()
+  const fileName = `${iFileName}DataSeeder`
+
+  const ts = Date.now()
+  const mFileName = `${ts}-${fileName}Table`
+  const className = `${fileName}Table${ts}`
+
+  const migrationFile = `${cwd}/src/migrations/${mFileName}.ts`
+  fs.copyFileSync(
+    process.env.HOME +
+      '/sites/personal/custom-personal-scripts/nestjs/seeder.stub',
+    migrationFile
+  )
+  let data: string = fs.readFileSync(migrationFile, { encoding: 'utf8' })
+  data = data
+    .replace(/#table#/g, tableName)
+    .replace(/#migrationclass#/g, className)
+  fs.writeFileSync(migrationFile, data)
+
+  exec(`code -r ${migrationFile}`)
+}
+
+const getInput = () => {
+  const tableName: string = readlineSync.question(
+    'What is the name of the table : '
+  )
+  const fileName: string = readlineSync.question(
+    'name of the migration file : '
+  )
+
+  writeMigrationFile(tableName, fileName)
+}
+
+getInput()
