da302e564601f9716267c8a1d1ac8405c797e52b
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-09-07 09:37:00 +0545

    listing implemented in node

---
+++ lib/listing.ts
@@ -0,0 +1,53 @@
+import * as readlineSync from 'readline-sync'
+import * as fs from 'fs'
+import { exec } from 'child_process'
+
+// get inputs from the user against the required stuffs
+const entity: string = readlineSync.question('Name of the Entity : ')
+const dir: string = readlineSync.question('Name of the module : ')
+
+console.log(entity)
+console.log(dir)
+
+// convert the given inputs to required converted variables
+const timestamp = Date.now()
+const file = entity
+  .replace(/[A-Z]/g, (match) => `.${match}`)
+  .replace(/^./g, '')
+  .toLowerCase()
+const sfile = entity.charAt(0).toLowerCase() + entity.slice(1)
+
+// create directories and move to the working directory
+const dirNames = ['dtos', 'libraries']
+
+for (const dirName of dirNames) {
+  const dirToMake = `src/${dir}/${dirName}`
+  if (!fs.existsSync(dirToMake)) fs.mkdirSync(dirToMake, { recursive: true })
+}
+
+// process the dto stuff
+const dtoFile = `./src/${dir}/dtos/${file}.list.filter.dto.ts`
+fs.copyFileSync(
+  process.env.HOME +
+    '/sites/personal/custom-personal-scripts/nestjs/listing.dto.stub',
+  dtoFile
+)
+let data = fs.readFileSync(dtoFile, { encoding: 'utf8' })
+data = data.replace(/#entity#/g, entity)
+fs.writeFileSync(dtoFile, data)
+console.log(`dto file : ${file}.list.filter.dto.ts`)
+
+// process the processing listing stuff
+const processFile = `./src/${dir}/libraries/process.${file}.list.ts`
+fs.copyFileSync(
+  process.env.HOME +
+    '/sites/personal/custom-personal-scripts/nestjs/process.list.stub',
+  processFile
+)
+data = fs.readFileSync(processFile, { encoding: 'utf8' })
+data = data.replace(/#entity#/g, entity)
+fs.writeFileSync(processFile, data)
+console.log(`process listing file : process.${file}.list.ts`)
+
+exec(`code -r ${dtoFile}`)
+exec(`code -r ${processFile}`)
