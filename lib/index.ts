111a9b80cbf5c3c4de5395bca4bd10463fc8dc5f
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-09-08 11:51:17 +0545

    bug fix

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
 import { Command } from 'commander'
 import { exec } from 'child_process'
+import * as path from 'path'
 
 const program = new Command()
+const scriptDirectory = path.join(__dirname, '../dist')
 
 program.version('1.0.0').description('CLI Tool for generating nestjs files')
 
@@ -9,52 +11,64 @@ program
   .command('generator')
   .description('Run generator script')
   .action(() => {
-    exec(`node dist/generator.js`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing generator: ${error.message}`)
-        process.exit(1)
+    exec(
+      `node ${path.join(scriptDirectory, 'generator.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing generator: ${error.message}`)
+          process.exit(1)
+        }
+        console.log(stdout)
       }
-      console.log(stdout)
-    })
+    )
   })
 
 program
   .command('listing')
   .description('Run listing script')
   .action(() => {
-    exec(`node dist/listing.js`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing listing: ${error.message}`)
-        process.exit(1)
+    exec(
+      `node ${path.join(scriptDirectory, 'listing.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing listing: ${error.message}`)
+          process.exit(1)
+        }
+        console.log(stdout)
       }
-      console.log(stdout)
-    })
+    )
   })
 
 program
   .command('migrator')
   .description('Run migrator script')
   .action(() => {
-    exec(`node dist/migrator.js`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing migrator: ${error.message}`)
-        process.exit(1)
+    exec(
+      `node ${path.join(scriptDirectory, 'migrator.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing migrator: ${error.message}`)
+          process.exit(1)
+        }
+        console.log(stdout)
       }
-      console.log(stdout)
-    })
+    )
   })
 
 program
   .command('seeder')
   .description('Run seeder script')
   .action(() => {
-    exec(`node dist/seeder.js`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing seeder: ${error.message}`)
-        process.exit(1)
+    exec(
+      `node ${path.join(scriptDirectory, 'seeder.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing seeder: ${error.message}`)
+          process.exit(1)
+        }
+        console.log(stdout)
       }
-      console.log(stdout)
-    })
+    )
   })
 
 if (!process.argv.slice(2).length) {
